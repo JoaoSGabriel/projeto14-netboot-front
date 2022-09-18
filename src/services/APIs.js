9855319283990883837fbb3eb753110e3d07f6fe
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-09-18 19:31:52 -0300

    implement some features

--- src/services/APIs.js
+++ src/services/APIs.js
@@ -1,25 +1,27 @@
 import axios from "axios";
 
+const BASE_URL = "https://netbootback.herokuapp.com";
+
 async function removeCartProduct(id, config) {
   const promise = await axios.delete(
-    `http://localhost:5000/cart/${id}`,
+    `${BASE_URL}/cart/${id}`,
     config
   );
   return promise;
 }
 
 async function addCartProduct(config) {
-  const promise = await axios.post(`http://localhost:5000/cart`, config);
+  const promise = await axios.post(`${BASE_URL}/cart`, config);
   return promise;
 }
 
 async function getCartProducts(config) {
-  const promise = await axios.get(`http://localhost:5000/cart`, config);
+  const promise = await axios.get(`${BASE_URL}/cart`, config);
   return promise;
 }
 
 async function cleanCart(config) {
-  const promise = await axios.delete(`http://localhost:5000/cleanCart`, config);
+  const promise = await axios.delete(`${BASE_URL}/cleanCart`, config);
   return promise;
 }
 
